422177b3173aaa2a5778e53c0cf0a1a89e7a6e40
Eben <me@ebenroux.co.za>
2024-07-30 19:36:12 +0200

    Package upgrade.

--- Shuttle.Esb.Logging/InboxMessagePipelineObserver.cs
+++ Shuttle.Esb.Logging/InboxMessagePipelineObserver.cs
@@ -3,7 +3,6 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Shuttle.Core.Contract;
 using Shuttle.Core.Pipelines;
-using Shuttle.Core.PipelineTransaction;
 
 namespace Shuttle.Esb.Logging
 {
@@ -18,14 +17,11 @@ namespace Shuttle.Esb.Logging
         IPipelineObserver<OnAfterDecryptMessage>,
         IPipelineObserver<OnDeserializeMessage>,
         IPipelineObserver<OnAfterDeserializeMessage>,
-        IPipelineObserver<OnStartTransactionScope>,
         IPipelineObserver<OnEvaluateMessageHandling>,
         IPipelineObserver<OnAfterEvaluateMessageHandling>,
         IPipelineObserver<OnProcessIdempotenceMessage>,
         IPipelineObserver<OnHandleMessage>,
         IPipelineObserver<OnAfterHandleMessage>,
-        IPipelineObserver<OnCompleteTransactionScope>,
-        IPipelineObserver<OnDisposeTransactionScope>,
         IPipelineObserver<OnSendDeferred>,
         IPipelineObserver<OnAfterSendDeferred>,
         IPipelineObserver<OnAcknowledgeMessage>,
@@ -149,16 +145,6 @@ namespace Shuttle.Esb.Logging
             await Trace(pipelineEvent);
         }
 
-        public void Execute(OnCompleteTransactionScope pipelineEvent)
-        {
-            Trace(pipelineEvent).GetAwaiter().GetResult();
-        }
-
-        public async Task ExecuteAsync(OnCompleteTransactionScope pipelineEvent)
-        {
-            await Trace(pipelineEvent);
-        }
-
         public void Execute(OnDecompressMessage pipelineEvent)
         {
             Trace(pipelineEvent).GetAwaiter().GetResult();
@@ -195,16 +181,6 @@ namespace Shuttle.Esb.Logging
         }
 
         public async Task ExecuteAsync(OnDeserializeTransportMessage pipelineEvent)
-        {
-            await Trace(pipelineEvent);
-        }
-
-        public void Execute(OnDisposeTransactionScope pipelineEvent)
-        {
-            Trace(pipelineEvent).GetAwaiter().GetResult();
-        }
-
-        public async Task ExecuteAsync(OnDisposeTransactionScope pipelineEvent)
         {
             await Trace(pipelineEvent);
         }
@@ -247,16 +223,6 @@ namespace Shuttle.Esb.Logging
         }
 
         public async Task ExecuteAsync(OnSendDeferred pipelineEvent)
-        {
-            await Trace(pipelineEvent);
-        }
-
-        public void Execute(OnStartTransactionScope pipelineEvent)
-        {
-            Trace(pipelineEvent).GetAwaiter().GetResult();
-        }
-
-        public async Task ExecuteAsync(OnStartTransactionScope pipelineEvent)
         {
             await Trace(pipelineEvent);
         }
